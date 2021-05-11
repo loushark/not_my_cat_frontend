3e78fbfa32b701bf429e78423a1acf5762a95fd6
Lou Reade <39278690+loushark@users.noreply.github.com>
2021-05-11 12:01:34 +0100

    set up axios and jest, passed a guard test to check jest is set up correctly and working
    Co-Authored-by: <dm_greaves@msn.com>

---
+++ src/App.spec.js
@@ -0,0 +1,8 @@
+//testing configuration of jest. can we run jest and pass this test:
+//keep this as guard test?
+
+describe('My Test Suite', () => {
+  it('My Test Case', () => {
+    expect(true).toEqual(true);
+  });
+});
