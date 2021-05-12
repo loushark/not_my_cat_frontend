244c7878f617f63b53fc467cf86e447179996b88
Lou Reade <39278690+loushark@users.noreply.github.com>
2021-05-12 11:57:26 +0100

    set up landing page with title and logo, tested
    Co-Authored-By: David Greaves <38595284+dmgreaves@users.noreply.github.com>

--- src/components/app/App.spec.js
+++ src/components/app/App.spec.js
@@ -6,10 +6,22 @@ describe('app', () => {
   const wrapper = shallow(<App />);
 
   it('should render a <div />', () => {
-    expect(wrapper.find('div').length).toEqual(2);
+    expect(wrapper.find('div').length).toEqual(3);
   })
 
   it('should have a header with class App-header', () => {
     expect(wrapper.exists('.App-header')).toEqual(true)
+  })
+
+  it('has a title', () => {
+    expect(wrapper.exists('h1')).toEqual(true)
+  })
+
+  it('has a logo', () => {
+    expect(wrapper.exists('.App-logo')).toEqual(true)
+  })
+
+  it('has a p statment when a user is not logged in', () => {
+    expect(wrapper.exists('.latest-cats')).toEqual(true)
   })
 })
