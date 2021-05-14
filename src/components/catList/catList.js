6a20570a3d21f22e7b300708495ba7c937dd8661
Stu Behan <33100049+StuBehan@users.noreply.github.com>
2021-05-14 17:58:57 +0100

    defaults to all cats

--- src/components/catList/catList.js
+++ src/components/catList/catList.js
@@ -1,32 +1,31 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import Cat from '../catCard/catCard.js'
 import useCats from '../../hooks/use-cats'
 import '../../App.css';
 import { AuthContext } from "../../App";
 
 const CatList = (state) => {
-  const [cats] = useCats();
-  let maxCats;
-  let threeCats;
+  const [cats] =  useCats();
+ 
+  // const catArray = useCats().slice(Math.max(cats.length - maxCats, 0));
+  // console.log(catArray)
 
-  useEffect( () => {
-    if(!state.isAuthenticated) {
-      maxCats = 3
-    } else {
-      maxCats = 10
-    }
-    threeCats = cats.slice(Math.max(cats.length - maxCats, 0));
-  }, [state.isAuthenticated]);
+  // const catArray = cats.slice(Math.max(cats.length - maxCats, 0));
+  
+  // useEffect( (catArray) => {
+  //   if(!state.isAuthenticated) {
+  //     maxCats.current = 3
+  //   } else {
+  //     maxCats.current = 10
+  //   }
+  // }, [state.isAuthenticated, cats]);
 
-
-  console.log(state)
- console.log(cats);
   return (
     <div className="cat-list">
-      { threeCats.map((cat)=> (
+      { cats.map((cat)=> (
           <Cat key={`cat-${cat._id}`} {...cat} />
         ))}
-      </div>
+    </div>
   );
 };
 
