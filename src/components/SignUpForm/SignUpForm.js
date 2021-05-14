67bead16bb849bde690a1ef0a1768fd07094ea4b
mstrivens <max.strivens@live.co.uk>
2021-05-14 16:18:49 +0100

    Sign up form sends post req to backend and throws error if email already exists in db

--- src/components/SignUpForm/SignUpForm.js
+++ src/components/SignUpForm/SignUpForm.js
@@ -3,37 +3,45 @@ import axios from 'axios';
 
 const SignUpForm = () => {
   const [userData, setUserData] = useState({
-    username: "",
-    email: "",
-    password: ""
+    _id: '',
+    email: '',
+    password: ''
   })
-  console.log(userData)
+  const [invalid, setInvalid] = useState(false)
+  const [signUp, setSignUp] = useState(false)
 
   const onChange = (element) => {
-    setUserData(() => ({[element.target.name]: element.target.value }))
+    setUserData((prevState) => ({...prevState, [element.target.name]: element.target.value }))
   }
 
   const signup = (element) => {
     element.preventDefault()
+    
     axios.post('http://localhost:8082/api/users', userData)
     .then(response => {
-      if (response.status === 200) {
-        console.log(response)
+      if (response.status !== 500) {
         console.log("Sign up complete")
+        setSignUp(true)
         return response   }
-      else {
-        console.log("Not sending")
-      }
-  })
+      })
+    .catch(error => {
+      setInvalid(true)
+      console.log(error)
+    })
 }
   return (
 
   <div>
     <h3>Sign up to add your cats!</h3>
-
+    {invalid && <div>
+      <strong>Email has already been used!</strong>
+    </div>}
+    {signUp && <div>
+      <strong>Sign-Up successful!</strong>
+    </div>}
     <form className="SignUp-form" onSubmit={element => signup(element)}>
       <input type="text"
-      name="username"
+      name="_id"
       placeholder="Username"
       onChange={element => onChange(element)}/>
       <br />
@@ -50,7 +58,7 @@ const SignUpForm = () => {
       onChange={element => onChange(element)} />
       <br />
       <br />
-      <input type="submit" className="button" onClick={element => signup(element)}/>
+      <input type="submit" className="button"/>
     </form>
     </div>
   );
