a89d719faacc3bf94978f40cd5ccf955eac0876c
mstrivens <max.strivens@live.co.uk>
2021-05-18 18:01:18 +0100

    SVG cat logo now reder but doesn't resize

--- src/components/MapContainer/MapContainer.js
+++ src/components/MapContainer/MapContainer.js
@@ -1,14 +1,25 @@
 import '../../App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { GoogleMap, LoadScript, Marker, InfoWindow } from '@react-google-maps/api';
 import useCats from '../../hooks/use-cats';
 import CatMapList from '../catMapList/catMapList.js'
 import Cat from '../catCard/catCard.js'
+import logo from '../../cat.svg';
 require ('dotenv/config');
+
 
 const MapContainer = () => {
   const [cats] = useCats();
   const [ selected, setSelected ] = useState({});
+  const [ currentPosition, setCurrentPosition ] = useState();
+
+  const success = position => {
+    const currentPosition = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
+    }
+    setCurrentPosition(currentPosition);
+  };
 
   const onSelect = cat => {
     setSelected(cat);
@@ -23,21 +34,28 @@ const MapContainer = () => {
     lat:51.514401, lng: -0.105196
   }
 
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(success);
+  })
+
   return (
     <LoadScript
-      googleMapsApiKey='AIzaSyCwHGfQTy_Eg2pSkOq-svnJCmB1f4fK54M'>
+      googleMapsApiKey="AIzaSyBwYbpWJFVS10cEqo1lh_qFRm79dMq0_a0">
       <GoogleMap
         mapContainerStyle={mapStyles}
         zoom={16}
-        center={defaultCenter}
+        center={currentPosition}
       >
       {cats.map(cat => {
         return (
+          <div className='marker'>
           <Marker
+            icon={logo}
             key={cat._id}
             position={cat.position}
             onClick={() => onSelect(cat)}
           />
+          </div>
         )
       })}
       {selected.position && (<InfoWindow
