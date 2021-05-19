2bd22ed3c91361c34ce985c5db5395a1f9723059
Stu Behan <33100049+StuBehan@users.noreply.github.com>
2021-05-20 00:04:39 +0100

    added crowns and other CSS changes

--- src/components/catCard/catCard.js
+++ src/components/catCard/catCard.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import SpottedButton from '../SpottedButton/SpottedButton.js'
 import '../../App.css';
+import crown1 from '../../svg/crown1outline.svg'
+import crown2 from '../../svg/crown2outline.svg'
+import crown3 from '../../svg/crown3outline.svg'
 import spareImg from '../../svg/cat1.svg'
 import { AuthContext } from '../../App'
 import { DeleteButton, PlayGameButton } from '../catCardButtons/catCardButtons.js'
@@ -23,11 +26,21 @@ const Cat =  ( { catName, user_id, cattitude, floof, chonk, image, wins, timesSp
     image = spareImg
   }
 
+  const crowns = () => {
+    if (wins > 10 && wins < 20) {
+      return crown1
+    } else if (wins > 20 && wins < 30) {
+      return crown2 
+    } else if (wins > 30) {
+      return crown3
+    }
+  }
+  
   return (
     <div className="Cat-id grow">
+      {wins > 10 ? <img className='Cat-crown' src={crowns()} alt='crown'/> : null}
       <div className="Cat-wins">
-      <img className='Cat-crown' src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Heraldic_eastern_crown.svg/1200px-Heraldic_eastern_crown.svg.png" alt='crown'/>
-        <p>Wins</p>
+        <p>WINS</p>
         <p>{wins}</p>
       </div>
       <header className="Cat-header">
